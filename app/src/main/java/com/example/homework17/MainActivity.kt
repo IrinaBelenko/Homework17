a7c535f80b206081774a68cdc4cd64c7e6deef44
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2024-02-09 10:15:48 +0200

    json from internet

--- app/src/main/java/com/example/homework17/MainActivity.kt
+++ app/src/main/java/com/example/homework17/MainActivity.kt
@@ -3,31 +3,33 @@ package com.example.homework17
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
-import com.google.gson.Gson
-import java.io.BufferedReader
-import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
-        val jsonFile = assets.open("all.json")
-        val bufferedReader = BufferedReader(InputStreamReader(jsonFile))
-        val stringBuilder = StringBuilder()
-        bufferedReader.useLines {
-                lines -> lines.forEach { stringBuilder.append(it) }
-        }
-        val jsonString = stringBuilder.toString()
-        val data = Gson().fromJson(jsonString, Heroes::class.java)
-        val items = data.heroes
-        val myAdapter = RecyclerViewAdapter(items as MutableList<Hero>) {
-            Toast.makeText(this, "${it} clicked", Toast.LENGTH_SHORT).show()
-        }
-        recyclerView.adapter = myAdapter
+        val api = ApiClient.client.create(ApiInterface::class.java)
+        api.getHeroes()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                if (it.isNotEmpty()) {
+                    val items = it
+                    val myAdapter = RecyclerViewAdapter(items as MutableList<Hero>) {
+                        Toast.makeText(this, "${it} clicked", Toast.LENGTH_SHORT).show()
+                    }
+                    recyclerView.adapter = myAdapter
+                }
+            }, {
+                Toast.makeText(this, "Fetch error ${it.message}", Toast.LENGTH_SHORT).show()
+            })
         recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
