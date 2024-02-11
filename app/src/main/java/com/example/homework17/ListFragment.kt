b1b9e2718cddc1a6830318050b95612a987219b0
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2024-02-11 18:08:49 +0200

    fix

--- app/src/main/java/com/example/homework17/ListFragment.kt
+++ app/src/main/java/com/example/homework17/ListFragment.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ListView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -35,8 +32,7 @@ class ListFragment : Fragment() {
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
                 if (it.isNotEmpty()) {
-                    val items = it
-                    val myAdapter = RecyclerViewAdapter(items as MutableList<Hero>) {
+                    val myAdapter = RecyclerViewAdapter(it as MutableList<Hero>) {
                         val detailsFragment = DetailsFragment()
                         detailsFragment.setDescription(it.images.lg, it.name, it.biography.firstAppearance)
                         parentFragmentManager.beginTransaction()
